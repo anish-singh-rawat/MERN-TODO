1b8657511998bec0078652368df22b340e031781
anish-singh-rawat <anishsinghrawat1>
2024-01-08 19:01:44 +0530

    crud operations

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,8 +1,31 @@
-
+import "./App.css"
+import Todo from "./component/Todo";
 function App() {
   return (
-   <>
-   </>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col d-flex justify-content-center align-items-center vh-100">
+            <div className="todo-header">
+              <div className="sub-todo-header">
+                <div className="top-heading">
+                  <center> <h3> Add Todo </h3></center>
+                  <br />
+                  <div className="d-flex justify-content-around">
+                  <input type="text" placeholder="Please Enter data" />
+                  <div className="btn btn-success">submit</div>
+                  </div>
+                </div>
+                <br />
+                <div className="list-data">
+                  <Todo text="hello"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
